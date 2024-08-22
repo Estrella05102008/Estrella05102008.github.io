2886b1f5f1f5151a8a299b2b8891bfaace73ee80
Estrella <samelyestrella41@gmail.com>
2024-08-21 20:14:12 -0500

    agregando archivos

---
+++ js/cifrado.js
@@ -0,0 +1,33 @@
+function descifrado_cifrado(texto, desplazamiento,accion){
+    const abecedario = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    let resultado = "";
+    desplazamiento = desplazamiento % abecedario.length;
+  
+    for (let i=0;i < texto.length; i++){
+        const letraActual = texto [i].toUpperCase();
+        const indiceActual = abecedario.indexOf(letraActual);
+  
+        if(indiceActual === -1){
+            resultado += letraActual;
+        }else{
+            let nuevoIndice
+            if (accion == 1){
+                nuevoIndice =(indiceActual + desplazamiento)%abecedario.length;
+            }else{
+                nuevoIndice =(indiceActual - desplazamiento)%abecedario.length;
+                if(nuevoIndice < 0){
+                    nuevoIndice += abecedario,length;
+                }
+            }
+            resultado += abecedario[nuevoIndice];
+        }
+    }
+    return resultado;
+  }
+  /*
+  let textoOriginal ="hola";
+  let desplazamiento = 3;
+  let textoCifrado = descifrado_cifrado(textoOriginal, desplazamiento,1);
+  console.log(textoCifrado);
+  */
+  export default descifrado_cifrado;
